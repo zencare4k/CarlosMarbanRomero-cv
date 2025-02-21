100653c650488b3afaa76078e99fe14614bf0d37
z4k23 <122319845+z4k23@users.noreply.github.com>
2025-02-21 20:27:00 +0100

    xd

--- src/components/intro/MediaContent.jsx
+++ src/components/intro/MediaContent.jsx
@@ -10,6 +10,13 @@ import backButtonImage from '../../assets/images/quit_button.png'; // Imagen del
 import replaceSound from '../../assets/sounds/Enter.wav'; // Importa el sonido
 import clickSound from '../../assets/sounds/Tap_Sound.wav'; // Importa el sonido de clic
 import backgroundImage from '../../assets/Background_Image/background_image.png'; // Importa la imagen de fondo
+import backSound from '../../assets/sounds/Exit.wav'; // Importa el sonido de retroceso
+import iconImage from '../../assets/icons/html.png'; // Importa el icono
+import iconImage2 from '../../assets/icons/CSS.png'; // Importa el icono
+import iconImage3 from '../../assets/icons/javascript.png'; // Importa el icono
+import iconImage4 from '../../assets/icons/nodeJS.png'; // Importa el icono
+import iconImage5 from '../../assets/icons/react.png'; // Importa el icono
+import iconImage6 from '../../assets/icons/mongoDB.png'; // Importa el icono
 import PropTypes from 'prop-types';
 
 const MediaContent = ({ handleImageClick }) => {
@@ -17,9 +24,11 @@ const MediaContent = ({ handleImageClick }) => {
   const [isBackgroundReplaced, setIsBackgroundReplaced] = useState(false);
   const [isFinalReplaced, setIsFinalReplaced] = useState(false); // Nuevo estado para las imágenes finales
   const [isBackButtonVisible, setIsBackButtonVisible] = useState(false); // Nuevo estado para el botón de retroceso
+  const [isReplaceButtonVisible, setIsReplaceButtonVisible] = useState(true); // Estado para controlar la visibilidad del botón de reemplazo
   const videoRef = useRef(null);
   const audioRef = useRef(null); // Referencia para el audio de reemplazo
   const clickAudioRef = useRef(null); // Referencia para el audio de clic
+  const backAudioRef = useRef(null); // Referencia para el audio de retroceso
 
   const handleClick = () => {
     if (!isReplaced) {
@@ -38,6 +47,7 @@ const MediaContent = ({ handleImageClick }) => {
     setIsBackgroundReplaced(true);
     setIsFinalReplaced(true); // Actualiza el estado para las imágenes finales
     setIsBackButtonVisible(true); // Muestra el botón de retroceso
+    setIsReplaceButtonVisible(false); // Oculta el botón de reemplazo
     if (audioRef.current) {
       audioRef.current.currentTime = 0; // Reinicia el audio
       audioRef.current.play().catch(error => {
@@ -51,6 +61,13 @@ const MediaContent = ({ handleImageClick }) => {
     setIsBackgroundReplaced(false); // Oculta la imagen de fondo reemplazada
     setIsFinalReplaced(false); // Restablece el estado de las imágenes finales
     setIsBackButtonVisible(false); // Oculta el botón de retroceso
+    setIsReplaceButtonVisible(true); // Muestra el botón de reemplazo
+    if (backAudioRef.current) {
+      backAudioRef.current.currentTime = 0; // Reinicia el audio
+      backAudioRef.current.play().catch(error => {
+        console.error('Error attempting to play sound', error);
+      });
+    }
   };
 
   return (
@@ -62,8 +79,33 @@ const MediaContent = ({ handleImageClick }) => {
             alt="Background"
             className="background-image"
           />
-          <div className="background-text">Java</div> {/* Añade el texto */}
-          <div className='background-text2'></div>
+          <div className="background-text">Habilidades</div>
+          <div className="additional-texts">
+            <div className="text-with-icon" id='arriba'>
+              <img src={iconImage} alt="Icon" className="icon"  />
+              <div className="text">HTML</div>
+            </div>
+            <div className="text-with-icon" id='arriba'>
+              <img src={iconImage2} alt="Icon" className="icon" />
+              <div className="text">CSS</div>
+            </div>
+            <div className="text-with-icon" id='arriba'>
+              <img src={iconImage3} alt="Icon" className="icon" />
+              <div className="text">JavaScript</div>
+            </div>
+            <div className="text-with-icon" id='abajo'>
+              <img src={iconImage4} alt="Icon" className="icon" />
+              <div className="text">Node Js</div>
+            </div>
+            <div className="text-with-icon" id='abajo'>
+              <img src={iconImage5} alt="Icon" className="icon" />
+              <div className="text">React Framework</div>
+            </div>
+            <div className="text-with-icon" id='abajo'>
+              <img src={iconImage6} alt="Icon" className="icon" />
+              <div className="text">mongoDB</div>
+            </div>
+          </div>
         </>
       )}
       {isReplaced ? (
@@ -84,14 +126,16 @@ const MediaContent = ({ handleImageClick }) => {
             onDragStart={(e) => e.preventDefault()}
             onContextMenu={(e) => e.preventDefault()} // Prevenir clic derecho
           />
-          <div className="replace-button" onClick={handleBackgroundReplace}>
-            <img
-              src={buttonImage}
-              alt="Reemplazar Imágenes"
-              className="replace-button-image"
-            />
-            <div className="replace-button-text">Habilidades</div> {/* Añade el texto */}
-          </div>
+          {isReplaceButtonVisible && (
+            <div className="replace-button" onClick={handleBackgroundReplace}>
+              <img
+                src={buttonImage}
+                alt="Reemplazar Imágenes"
+                className="replace-button-image"
+              />
+              <div className="replace-button-text">Habilidades</div> {/* Añade el texto */}
+            </div>
+          )}
           {isBackButtonVisible && (
             <img
               src={backButtonImage}
@@ -101,6 +145,7 @@ const MediaContent = ({ handleImageClick }) => {
             />
           )}
           <audio ref={audioRef} src={replaceSound} /> {/* Elemento de audio para el sonido de reemplazo */}
+          <audio ref={backAudioRef} src={backSound} /> {/* Elemento de audio para el sonido de retroceso */}
         </>
       ) : (
         <>
@@ -147,7 +192,6 @@ const MediaContent = ({ handleImageClick }) => {
 
 MediaContent.propTypes = {
   handleImageClick: PropTypes.func.isRequired,
-  isInitialImageVisible: PropTypes.bool.isRequired,
 };
 
 export default MediaContent;
